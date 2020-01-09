ea68cd08d9db78ce75fe432791e164913c16b582
keith ferney <keith.ferney@tireguru.net>
2020-01-09 09:46:22 -0700

    refined links

--- src/app/nav-bar/nav-links/nav-links.component.ts
+++ src/app/nav-bar/nav-links/nav-links.component.ts
@@ -3,10 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-links',
   templateUrl: './nav-links.component.html',
-  styleUrls: ['./nav-links.component.css']
+  styleUrls: ['./nav-links.component.css'],
+  host: { id: 'nav-links' }
 })
 export class NavLinksComponent implements OnInit {
-
+  links = [
+    {
+      name: 'Dashboard', 
+      href: '#'
+    },{
+      name: 'Hall',
+      href: '#'
+    },{
+      name: 'New',
+      href: '#'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
