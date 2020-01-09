11bfcbd7f181e36e497bc3c403154fda3df0946f
keith ferney <keith.ferney@tireguru.net>
2020-01-09 08:30:39 -0700

    getting tailwind working and adding nav links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavLinksComponent } from './nav-bar/nav-links/nav-links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    NavLinksComponent
   ],
   imports: [
     BrowserModule
