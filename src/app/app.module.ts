62ffd75ae1a0b9e3d72453dfb190fe0aefc6daa5
keith ferney <keith.ferney@tireguru.net>
2020-01-09 09:58:24 -0700

    scaffolding hero hall and dash components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NavLinksComponent } from './nav-bar/nav-links/nav-links.component';
+import { HallComponent } from './hall/hall.component';
+import { DashComponent } from './dash/dash.component';
+import { HeroComponent } from './hero/hero.component';
+import { NewComponent } from './hero/new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    NavLinksComponent
+    NavLinksComponent,
+    HallComponent,
+    DashComponent,
+    HeroComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule
